a16194c8f6af89099198181dff0bc7370d44b222
Greatness <Oluwolegreatnessadeola@gmail.com>
2026-09-16 11:56:49 +0100

    feat: update RSVP flow for shared wedding link

--- Backend/src/routes/rsvpRoutes.js
+++ Backend/src/routes/rsvpRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const rsvpController = require('../controllers/rsvpController');
 
-
-router.get('/:token', rsvpController.getRsvp);
-router.post('/:token', rsvpController.postRsvp);
+router.post('/:slug', rsvpController.postRsvp);
 
 module.exports = router;
